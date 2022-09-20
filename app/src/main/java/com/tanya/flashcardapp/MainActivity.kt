7f902a4afc733a7eebee9f52d5d5e8df0e7ce12c
Talia04 <tanyachisepo04@gmail.com>
2022-09-19 23:39:10 -0400

    These are the optional features: 1. Three answer choices 2. Answers are hidden initially, shown after toggle button is clicked 3. Answers change color when clicked, Red for wrong, green for correct

--- app/src/main/java/com/tanya/flashcardapp/MainActivity.kt
+++ app/src/main/java/com/tanya/flashcardapp/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.tanya.flashcardapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,20 +17,34 @@ class MainActivity : AppCompatActivity() {
 
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val flashcardOption1 = findViewById<TextView>(R.id.option1)
+        val flashcardOption2 = findViewById<TextView>(R.id.option2)
+        val flashcardOption3 = findViewById<TextView>(R.id.option3)
+        val answerVisibleToggle = findViewById<ImageView>(R.id.answers_visible_toggle)
 
-        findViewById<TextView>(R.id.flashcard_question).setOnClickListener{
+        //Log.i("Tanya", "Answer revealed")
 
-            flashcardQuestion.visibility = View.INVISIBLE
-            flashcardAnswer.visibility = View.VISIBLE
-            Toast.makeText(this, "Answer revealed", Toast.LENGTH_SHORT).show()
+        flashcardOption1.setOnClickListener{
 
-            Log.i("Tanya", "Answer revealed")
+            flashcardOption1.setBackgroundColor(Color.RED)
+            Toast.makeText(this, "Wrong Answer!", Toast.LENGTH_SHORT).show()
 
-            findViewById<TextView>(R.id.flashcard_answer).setOnClickListener{
+        }
+        flashcardOption2.setOnClickListener{
 
-                flashcardQuestion.visibility = View.VISIBLE
-                flashcardAnswer.visibility = View.INVISIBLE
-            }
+            Toast.makeText(this, "Correct Answer!", Toast.LENGTH_SHORT).show()
+            flashcardOption2.setBackgroundColor(Color.GREEN)
+        }
+        flashcardOption3.setOnClickListener{
+
+            Toast.makeText(this, "Wrong Answer!", Toast.LENGTH_SHORT).show()
+            flashcardOption3.setBackgroundColor(Color.RED)
+        }
+        answerVisibleToggle.setOnClickListener{
+            answerVisibleToggle.setImageResource(R.drawable.hide_answers)
+            flashcardOption1.visibility = View.VISIBLE
+            flashcardOption2.visibility = View.VISIBLE
+            flashcardOption3.visibility = View.VISIBLE
+        }
         }
     }
-}
